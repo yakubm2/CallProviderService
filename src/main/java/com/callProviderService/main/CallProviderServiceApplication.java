9f35af8950a05b95c30dd969ffab9cf53b3005c6
yakubm2 <yakubm2@gmail.com>
2023-03-15 00:46:50 +0530

    base code

---
+++ src/main/java/com/callProviderService/main/CallProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.callProviderService.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallProviderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/callProviderService/main/CallProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callProviderService.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
